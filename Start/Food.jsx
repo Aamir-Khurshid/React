0fcf07749ebf7783bc2ed5d46cbffc282d472f9c
Aamir Khurshid <108178409+Aamir-Khurshid@users.noreply.github.com>
2024-07-13 17:32:25 +0530

    Add files via upload

---
+++ Start/Food.jsx
@@ -0,0 +1,12 @@
+function Food(){
+    const food1 = "Apple";
+    const food2 = "Banana";
+
+    return(
+        <ul>
+            <li>{food1}</li>
+            <li>{food2}</li>
+        </ul>
+    )
+}
+export default Food
---
+++ Start/Header.jsx
@@ -0,0 +1,17 @@
+function Header(){
+
+    return(
+        <header>
+            <h1>My Website</h1>
+            <nav>
+                <ul>
+                    <li><a href=""></a>Home</li>
+                    <li><a href=""></a>Home</li>
+                    <li><a href=""></a>Home</li>
+                </ul>
+            </nav>
+            <hr></hr>
+        </header>
+    )
+}
+export default Header
