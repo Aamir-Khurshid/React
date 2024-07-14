7b2c792dcf76d230719350c1b8bdb97fb6dbc1dd
Aamir Khurshid <108178409+Aamir-Khurshid@users.noreply.github.com>
2024-07-14 13:36:32 +0530

    Update card.jsx

--- Card/card.jsx
+++ Card/card.jsx
@@ -1,9 +1,10 @@
 import profile from "./assets/Me.jpg"
+import style from "./index.module.css"
 
 function Card(){
     return(
         <div className="card">
-            <img className="cardImage" src= {profile} alt="AAMIR" />
+            <img className={style.card} src= {profile} alt="AAMIR" />
             <h2>Aamir Khurshid</h2>
             <p>I am currently a student.</p>
         </div>
