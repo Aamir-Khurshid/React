5249177f0d7e9d9bb2f14cd0e985a9803f12510c
Aamir Khurshid <108178409+Aamir-Khurshid@users.noreply.github.com>
2024-07-18 19:33:11 +0530

    Add files via upload

---
+++ Button/button.jsx
@@ -0,0 +1,7 @@
+import sd from "./button.module.css"
+function Button(){
+    return(
+        <button className={sd.button} >Click Me</button>
+    )
+}
+export default Button
---
+++ nutton2.jsx
@@ -0,0 +1,16 @@
+// Using inline Css
+
+
+function Nutton(){
+
+    const edit = {
+        backgroundColor:"blue",
+        borderRadius:"10%",
+        padding:"10px 20px"
+    }
+
+    return(
+        <button style={edit}>Dont Click</button>
+    )
+}
+export default Nutton
