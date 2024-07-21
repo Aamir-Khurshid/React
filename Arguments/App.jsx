44008f975ca27044c92ee617de8f561b9ac27f81
Aamir Khurshid <108178409+Aamir-Khurshid@users.noreply.github.com>
2024-07-21 17:02:40 +0530

    Update App.jsx

--- Arguments/App.jsx
+++ Arguments/App.jsx
@@ -3,8 +3,8 @@ import Student from "./student.jsx"
 function App() {
  return(
   <>
-  <Student name="AK Coder" age="21" isStudent={true}/>
-  <Student name="AK Noob" age="21" isStudent={false}/>
+  <Student name="AK Coder" age={21} isStudent={true}/>
+  <Student name="AK Noob" age={21} isStudent={false}/>
   </>
  )
 }
