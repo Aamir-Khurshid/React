c48fd9d30b5f6bcfe371a34bdcf1700ac33f8adf
Aamir Khurshid <108178409+Aamir-Khurshid@users.noreply.github.com>
2024-07-21 17:06:10 +0530

    Add files via upload

---
+++ Arguments With Specified Type/App.jsx
@@ -0,0 +1,11 @@
+import Student from "./student"
+function App() {
+ return(
+  <>
+  <Student name="AK Coder" age={21} isStudent={true}/>
+  <Student name="AK Noob" age={21} isStudent={false}/>
+  </>
+ )
+}
+
+export default App
